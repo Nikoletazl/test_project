d785c7a19c9923894f86eecadbd98b723db7e39a
Nikoleta Zlateva <87802876+Nikoletazl@users.noreply.github.com>
2022-03-29 20:36:31 +0300

    changed settings

--- test_project/settings.py
+++ test_project/settings.py
@@ -62,7 +62,7 @@ DEFAULT_DATABASE_CONFIG = {
     'NAME': os.getenv('DB_NAME', 'test_project_db'),
     'USER': os.getenv('DB_USER', 'postgres'),
     'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
-    'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+    'HOST': os.getenv('DB_HOST', 'test-project-app2.herokuapp.com'),
     'PORT': os.getenv('DB_PORT', '5432'),
 
 }
